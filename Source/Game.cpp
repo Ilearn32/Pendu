53c7d44f5ce612b13c9b61b29f7b73525412efe6
Zhenyu WU <zhen@outlook.fr>
2016-03-21 19:00:15 +0100

    Added files via upload
    randomWord.cpp: update performances

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,26 +1,10 @@
-/*
- * Copyright (c) 2016 WU Zhenyu
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
- * associated documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
- * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
- * conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 #include <iostream>
 #include <cctype>
 #include "Game.hpp"
 #include "Console.hpp"
 
 #undef max
+#undef min
 
 Game::Game::Game(std::string const& word, unsigned int counter) : m_word{ word }, m_counter{ counter }, m_found(word.size(), false)
 {
@@ -40,7 +24,7 @@ bool Game::Game::runGame()
 		if (!updateFounded(c)) // the character isn't in the word
 		{
 			std::cout << Console::accentPrintableStr("\"") << c << 
-				Console::accentPrintableStr("\"") << Console::accentPrintableStr(" ne se trouve pas dans le mot ou  a déjà été trouvé.") << 
+				Console::accentPrintableStr("\"") << Console::accentPrintableStr(" ne se trouve pas dans le mot ou a déjà été trouvé.") << 
 				std::endl;
 
 			--m_counter;
@@ -67,7 +51,7 @@ void Game::Game::printWord() const
 char Game::Game::getChar() const
 {
 	char c{};
-	while (!(std::cin >> c) || !std::isalpha(c))
+	while (!(std::cin >> c) || (!std::isalpha(c) && c != '-'))
 	{
 		std::cerr << Console::accentPrintableStr("Erreur de saisis !") << std::endl;
 		if (std::cin.eof())
--- Source/randomWord.cpp
+++ Source/randomWord.cpp
@@ -1,20 +1,3 @@
-/*
- * Copyright (c) 2016 WU Zhenyu
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
- * associated documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
- * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
- * conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
- * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
- * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 #include <vector>
 #include <fstream>
 #include "Random.hpp"
@@ -24,6 +7,8 @@
 std::string Utilities::randomWordFromFile(std::string const& filename)
 {
 	std::vector<std::string> words;
+	words.reserve(350'000);
+
 	std::ifstream file{ filename };
 	if (!file)
 		throw std::runtime_error{ Console::accentPrintableStr("File cannot be opened").c_str() };
